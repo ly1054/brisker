8780563914f1cd77ca6a77a5674dddbc7e333a72
ly1054 <ly1054@sohu.com>
2017-02-24 12:33:27 +0800

    首次提交

--- brisker-sample/src/main/java/com/ly1054/test/MainActivity.java
+++ brisker-sample/src/main/java/com/ly1054/test/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Brisker.inject(this);
+        mTextView1.setText("come on,baby!");
     }
 
 }
